379623796fdfb5178b1c7ac49513c1340cd7b2be
Lewis <shuaicode@gmail.com>
2022-04-13 17:30:33 +0800

    fix

---
+++ config/routes.ts
@@ -0,0 +1,40 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+  {
+    name: '节点管理',
+    icon: 'table',
+    path: '/nodes',
+    component: './nodes',
+  }
+];
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,29 @@
+import { useIntl } from 'umi';
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-layout';
+
+const Footer: React.FC = () => {
+  const intl = useIntl();
+  const defaultMessage = intl.formatMessage({
+    id: 'app.copyright.produced',
+    defaultMessage: 'Lewis Xingwen',
+  });
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/luxingwen/ospApp',
+          blankTarget: true,
+        }
+      ]}
+    />
+  );
+};
+
+export default Footer;
