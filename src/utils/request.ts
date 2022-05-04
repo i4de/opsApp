a394cc6f3671e3f81f8a2c6c3a035f3560f83242
Lewis <shuaicode@gmail.com>
2022-05-04 09:43:10 -0700

    feat(requet): hook response

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,29 +4,33 @@
  */
 import { notification } from 'antd';
 import { extend } from 'umi-request';
+import { history } from 'umi';
+import { stringify } from 'querystring';
 
 /**
  * 异常处理程序
  */
 const errorHandler = (error: { response: Response }): Response => {
-
   const { response } = error;
 
   if (response && response.status) {
-    response.clone().text().then(v => {
-      try {
-        const data = JSON.parse(v)
-        notification.error({
-          message: `请求错误, 错误码:${data.code}`,
-          description: data.message || data.msg,
-        });
-      } catch {
-        notification.error({
-          message: `请求错误, 错误码:${response.status}`,
-          description: v,
-        });
-      }
-    })
+    response
+      .clone()
+      .text()
+      .then((v) => {
+        try {
+          const data = JSON.parse(v);
+          notification.error({
+            message: `请求错误, 错误码:${data.code}`,
+            description: data.message || data.msg,
+          });
+        } catch {
+          notification.error({
+            message: `请求错误, 错误码:${response.status}`,
+            description: v,
+          });
+        }
+      });
   } else if (!response) {
     notification.error({
       description: '您的网络发生异常，无法连接服务器',
@@ -34,31 +38,26 @@ const errorHandler = (error: { response: Response }): Response => {
     });
   }
 
-
   return response;
 };
 
-
 const getAccessToken = () => {
   return localStorage.getItem('GO-OPS-X-TOKEN');
-}
-
+};
 
 const authInterceptor = (url: string, options: any) => {
-
-  const accessToken = getAccessToken()
+  const accessToken = getAccessToken();
   if (accessToken) {
     if (options.headers) {
-      (options.headers['GO-OPS-X-TOKEN'] = 'Bearer ' + accessToken)
+      options.headers['GO-OPS-X-TOKEN'] = 'Bearer ' + accessToken;
     }
   }
-
 
   return {
     url,
     options: { ...options },
   };
-}
+};
 
 /**
  * 配置request请求时的默认参数
@@ -69,7 +68,7 @@ const request = extend({
   errorHandler, // 默认错误处理
   credentials: 'include', // 默认请求是否带上cookie
   errorConfig: {
-    adaptor: (resData:any) => {
+    adaptor: (resData: any) => {
       return {
         ...resData,
         success: resData.ok,
@@ -79,44 +78,56 @@ const request = extend({
   },
 });
 
-const responseInterceptors = async (response: any, options: any) => {
-  response.clone().text().then(v => {
-    const data = JSON.parse(v)
-    if (data?.data?.msg !== 'OK' && data?.data?.msg) {
-      notification.error({
-        message: `请求错误, 错误码:500`,
-        description: data?.data?.msg,
-      });
-    } else {
-      // notification.success({
-      //   message: `请求成功, 状态码:200`,
-      //   description: data?.data?.msg,
-      // });
-    }
-  })
-  return response
-}
-
+const responseInterceptors = async (response: any) => {
+  console.log("---->");
+  response
+    .clone()
+    .text()
+    .then((v) => {
+      const data = JSON.parse(v);
+      console.log("response -->", data);
+      if (data?.code == 401) {
+        console.log("40111111111->", data)
+        notification.error({
+          message: `请求错误, 错误码:401`,
+          description: data?.data?.msg,
+        });
+        const { query = {}, search, pathname } = history.location;
+        const { redirect } = query;
+        // Note: There may be security issues, please note
+        if (window.location.pathname !== '/user/login' && !redirect) {
+          history.replace({
+            pathname: '/user/login',
+            search: stringify({
+              redirect: pathname + search,
+            }),
+          });
+        }
+      }
+      if (data?.data?.msg !== 'OK' && data?.data?.msg) {
+        notification.error({
+          message: `请求错误, 错误码:500`,
+          description: data?.data?.msg,
+        });
+      } else {
+        // notification.success({
+        //   message: `请求成功, 状态码:200`,
+        //   description: data?.data?.msg,
+        // });
+      }
+    });
+  return response;
+};
 
 export const stream = extend({
   credentials: 'include',
-  parseResponse: false
-})
+  parseResponse: false,
+});
 
-stream.interceptors.request.use(
-  authInterceptor,
-  { global: false }
-)
+stream.interceptors.request.use(authInterceptor, { global: false });
 
-request.interceptors.response.use(
-  responseInterceptors,
-  { global: false }
-);
+request.interceptors.response.use(responseInterceptors, { global: false });
 
-request.interceptors.request.use(
-  authInterceptor,
-  { global: false }
-); // second paramet defaults { global: true }
+request.interceptors.request.use(authInterceptor, { global: false }); // second paramet defaults { global: true }
 
 export default request;
-
